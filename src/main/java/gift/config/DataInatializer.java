440a9e606cc92aa094f8ace3799c6bd014d3be8d
hwangyungyu <vlzkcbdmdkr@naver.com>
2025-07-16 16:56:00 +0900

    feat(WishRestControllerTest) : add testcode wishlist

--- src/main/java/gift/config/DataInatializer.java
+++ src/main/java/gift/config/DataInatializer.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class DataInitializer {
+public class DataInatializer {
 
     @Bean
     public CommandLineRunner initData(MemberRepository memberRepository, ProductRepository productRepository) {
--- src/main/java/gift/request/TokenResponse.java
+++ src/main/java/gift/request/TokenResponse.java
@@ -5,10 +5,12 @@ import gift.Entity.Member;
 public class TokenResponse {
     private String token;
     private Member member;
+    private String role;
 
     public TokenResponse(String token, Member member) {
         this.token = token;
         this.member = member;
+        this.role = member.getRole();
     }
 
     public String getToken() {
@@ -17,4 +19,5 @@ public class TokenResponse {
     public Member getMember() {
         return member;
     }
+    public String getRole() {return role;}
 }
